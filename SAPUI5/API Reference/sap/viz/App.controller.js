14545b8eef765e788a3241980553129a6ca0e62c
张梦思 <547151644@qq.com>
2022-12-05 01:06:34 +0800

    viz

--- SAPUI5/API Reference/sap/viz/App.controller.js
+++ SAPUI5/API Reference/sap/viz/App.controller.js
@@ -3,11 +3,10 @@ sap.ui.define([
     'sap/ui/model/json/JSONModel'
 ], function (Controller, JSONModel) {
     return Controller.extend('zms.App', {
-        dataPath : "https://sapui5.hana.ondemand.com/test-resources/sap/viz/demokit/dataset/milk_production_testing_data/date_revenue_cost",
         onInit: function () {
             var oVizFrame = this.oVizFrame = this.getView().byId("idVizFrame");
             // oVizFrame.setVizProperties(this.settingsModel.chartType.values[3].vizProperties);
-            var dataModel = new JSONModel(this.dataPath + "/column/timeAxis.json");
+            var dataModel = new JSONModel("./timeAxis.json");
             oVizFrame.setModel(dataModel);
         },
         onPress: function () {
