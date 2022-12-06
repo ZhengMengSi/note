9a75042cb7139abc90561ef3cedee427d2ca275b
张梦思 <547151644@qq.com>
2022-12-06 16:43:56 +0800

    Step 30: Routing and Navigation

---
+++ SAPUI5/Documentation/3. Get Started Setup, Tutorials, and Demo Apps/3.2 Walkthrough1/Step 30 Routing and Navigation/webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,42 @@
+// 参考资料
+//
+// https://www.cnblogs.com/xiaoshiwang/p/15236633.html
+
+sap.ui.define([
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel',
+    '../model/formatter',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator'
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
+    "use strict";
+
+    return Controller.extend('zms.controller.InvoiceList', {
+        formatter: formatter,
+        onInit: function () {
+            var oViewModel = new JSONModel({
+                currency: 'EUR'
+            })
+            this.getView().setModel(oViewModel, 'view')
+        },
+        onFilterInvoices: function (oEvent) {
+            var aFilter = [];
+            var sQuery = oEvent.getParameter("query");
+            if (sQuery) {
+                aFilter.push(new Filter('ProductName', FilterOperator.Contains, sQuery))
+            }
+
+            var oList = this.byId('invoiceList') // sap.ui.core.Element
+            console.log(oList)
+            var oBinding = oList.getBinding('items') // sap.ui.model.Binding | sap.ui.model.ClientListBinding
+            console.log(oBinding.constructor)
+            oBinding.filter(aFilter)
+        },
+        onPress: function (oEvent) {
+            // console.log(this.getOwnerComponent()) // ManagedObject zms.Component#container-zms
+            var oRouter = this.getOwnerComponent().getRouter();
+            console.log(oRouter) // EventProvider sap.m.routing.Router
+            oRouter.navTo("detail");
+        }
+    })
+})
