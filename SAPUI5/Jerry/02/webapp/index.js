7dc91bba284fd86feb0491e346301501b0f04b10
张梦思 <547151644@qq.com>
2023-05-20 15:16:58 +0800

    1

--- SAPUI5/Jerry/02/webapp/index.js
+++ SAPUI5/Jerry/02/webapp/index.js
@@ -1,5 +1,9 @@
-sap.ui.define([
+/* sap.ui.define([
 ], function() {
     'use strict';
     alert("UI5 库文件准备就绪！")
-});
+}); */
+
+const script = document.querySelector('script')
+console.log(script);
+
