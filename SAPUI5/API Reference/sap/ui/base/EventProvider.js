075281f3f9f19a5748a15e54ff15fe599d942734
张梦思 <547151644@qq.com>
2023-05-22 19:13:04 +0800

    1

---
+++ SAPUI5/API Reference/sap/ui/base/EventProvider.js
@@ -0,0 +1,11 @@
+var e = new sap.ui.base.EventProvider()
+
+/******************************************* Methods *******************************************/
+// attachEvent
+// attachEventOnce
+// destroy
+// detachEvent
+// fireEvent
+
+/******************************************* Subclasses *******************************************/
+// sap.ui.base.ManagedObject
