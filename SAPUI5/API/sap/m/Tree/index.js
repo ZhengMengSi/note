e82a00d6d8a782ad7b9de5614d242a8c7a7f1adb
张梦思 <547151644@qq.com>
2023-05-24 15:46:36 +0800

    InteractiveDonutChart

--- SAPUI5/API/sap/m/Tree/index.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-    'sap/ui/core/mvc/XMLView'
-], function (XMLView) {
-    XMLView.create({
-        viewName: 'tree.App'
-    }).then(function (oView) {
-        oView.placeAt('content')
-    })
-})
