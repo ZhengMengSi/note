39bcc99c1b1f7ea12833f0c8055d543bebdd8125
张梦思 <547151644@qq.com>
2023-05-29 15:05:38 +0800

    1

--- JS/study-echarts/calc.js
+++ JS/study-echarts/calc.js
@@ -1,12 +1,17 @@
 var {
     getFinancialIndicatorList: financialIndicatorList,
     getAlertSettingsList: alertSettingList,
+    getGroupList: groupList,
 } = require('./test')
 
 /* var res = alertSettingList.data.alertSettingsDtos.map(item => item.objType)
 console.log(res.length);
 console.log(res); */
 
-var res = financialIndicatorList.data.map(item => item.fiName)
+/* var res = financialIndicatorList.data.map(item => item.fiName)
+console.log(res.length);
+console.log(res); */
+
+var res = groupList.data.groupRelationDtos.map(item => `${item.id}-${item.name}`)
 console.log(res.length);
 console.log(res);
